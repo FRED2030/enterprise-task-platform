dcaa169d5bd9fdf271595a8a62c0bc532ded4070
Ubuntu <ubuntu@ip-172-31-42-139.ap-northeast-1.compute.internal>
2026-04-26 01:40:35 +0000

    Jenkinsfile update

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,125 @@
-output "vpc_id" {
-  value = aws_vpc.task_platform_vpc.id
+provider "aws" {
+  region = "ap-northeast-1" # Tokyo
+}
+
+# -----------------------------
+# VPC
+# -----------------------------
+resource "aws_vpc" "task_platform_vpc" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "task-platform-vpc"
+  }
+}
+
+# -----------------------------
+# Internet Gateway
+# -----------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.task_platform_vpc.id
+
+  tags = {
+    Name = "task-platform-igw"
+  }
+}
+
+# -----------------------------
+# Public Subnet
+# -----------------------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.task_platform_vpc.id
+  cidr_block              = "10.10.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "task-platform-public-subnet"
+  }
+}
+
+# -----------------------------
+# Route Table
+# -----------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.task_platform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "task-platform-public-rt"
+  }
+}
+
+# -----------------------------
+# Route Table Association
+# -----------------------------
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# -----------------------------
+# Security Group
+# -----------------------------
+resource "aws_security_group" "task_platform_sg" {
+  name        = "task-platform-sg"
+  description = "Allow SSH and App"
+  vpc_id      = aws_vpc.task_platform_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App Port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "task-platform-sg"
+  }
+}
+
+# -----------------------------
+# EC2 Instance
+# -----------------------------
+resource "aws_instance" "task_platform_server" {
+  ami           = "ami-0d52744d6551d851e"
+  instance_type = "t2.micro"
+
+  key_name = "task-platform-key"
+
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.task_platform_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "task-platform-server"
+  }
+}
+
+# -----------------------------
+# OUTPUT
+# -----------------------------
+output "public_ip" {
+  value = aws_instance.task_platform_server.public_ip
 }
--- infrastructure/terraform/subnet.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.task_platform_vpc.id
-  cidr_block             = "10.10.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "task-platform-public-subnet"
-  }
-}
